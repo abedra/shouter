767196f20f7eb036a1555a721daaab6bc7d0a322
Aaron Bedra <aaron@aaronbedra.com>
2010-11-12 08:47:39 -0500

    adding logging wrappers

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [log4j/log4j "1.2.16"]
                  [postgresql/postgresql "8.4-701.jdbc4"]
                  [ring/ring-jetty-adapter "0.3.1"]
                  [compojure "0.5.2"]
--- src/shouter/main.clj
+++ src/shouter/main.clj
@@ -1,10 +1,23 @@
 (ns shouter.main
-  (:use [ring.adapter.jetty :only [run-jetty]]
+  (:use [clojure.pprint]
+        [clojure.contrib.logging :only [debug]]
+        [ring.adapter.jetty :only [run-jetty]]
 	[ring.middleware.params :only (wrap-params)]        
 	[ring.middleware.keyword-params :only [wrap-keyword-params]]
         [compojure.core :only [defroutes]])
   (:require [compojure.route :as route]
             [shouter.controllers.shouts]))
+
+(def static-file-regex
+     #".*\.(css|js|png|jpg|gif|ico)$")
+
+(defn wrap-logging [handler prefix]
+  (fn [request]
+    (if (re-find static-file-regex (:uri request))
+      (debug (str prefix (:uri request)))
+      (debug (str prefix " " (:request-method request) " " (:uri request)
+		  "\n\tParameters: " (with-out-str (pprint (:params request))))))
+    (handler request)))
 
 (defroutes routes
   shouter.controllers.shouts/routes
@@ -12,6 +25,7 @@
   (route/not-found "Not Found"))
 
 (def application (-> routes
+                     (wrap-logging "")
                      wrap-keyword-params
                      wrap-params))
 
@@ -19,3 +33,5 @@
   []
   (run-jetty (var application) {:port 8080
                                 :join? false}))
+
+(defn -main [] (start))

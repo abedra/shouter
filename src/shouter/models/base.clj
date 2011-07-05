7355a908dd2fec0973ea2b94907ceb136301b8cf
Mark McGranaghan <mmcgrana@gmail.com>
2011-07-04 18:13:03 -0700

    generalize url parsing

--- src/shouter/models/base.clj
+++ src/shouter/models/base.clj
@@ -4,16 +4,18 @@
   (:import (java.net URI)))
 
 (defn database-resource []
-  (let [url (URI. (or (System/getenv "DATABASE_URL") nil))
-        user-info (str/split (.getUserInfo url) #":")
-        database-name (last (str/split (str url) #"/"))]
-    {:username (first user-info)
-     :password (second user-info)
-     :subname (str "//" (.getHost url) ":5432/" database-name)}))
+  (let [url (URI. (System/getenv "DATABASE_URL"))
+        host (.getHost url)
+        port (if (pos? (.getPort url)) (.getPort url) 5432)
+        path (.getPath url)]
+    (merge
+      {:subname (str "//" host ":" port path)}
+      (if-let [user-info (.getUserInfo url)]
+        {:user (first (str/split user-info #":"))
+         :password (second (str/split user-info #":"))}))))
 
-(let [resource (database-resource)]
-  (def db {:classname "org.postgresql.Driver"
-           :subprotocol "postgresql"
-           :subname (:subname resource)
-           :user (:username resource)
-           :password (:password resource)}))
+(def db
+  (merge
+    {:classname "org.postgresql.Driver"
+     :subprotocol "postgresql"}
+    (database-resource)))

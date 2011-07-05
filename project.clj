1d794923b22886460eb560b7dcc2d02db1b9328e
Mark McGranaghan <mmcgrana@gmail.com>
2011-07-04 21:59:56 -0700

    bump project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject shouter "1.0.0-SNAPSHOT"
+(defproject shouter "0.0.1"
   :description "SHOUT from the webtops"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/java.jdbc "0.0.3-SNAPSHOT"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
-                 [ring/ring-jetty-adapter "0.3.9"]
-                 [compojure "0.6.3"]
-                 [hiccup "0.3.5"]]
+                 [ring/ring-jetty-adapter "0.3.10"]
+                 [compojure "0.6.4"]
+                 [hiccup "0.3.6"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :main shouter.main
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
